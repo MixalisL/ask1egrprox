5d4604b5007287d336b3876bd404d2cf0bee6404
MixalisL <83769471+MixalisL@users.noreply.github.com>
2023-11-07 13:06:01 +0200

    Add files via upload

--- ask1progr.c
+++ ask1progr.c
@@ -1,104 +1,24 @@
 #include <stdio.h>
 #define N 3
 int x=0, metr=0;
-int main();
-struct filmain
-{
+struct filmain{
     int code;
     int year;
     char title[25];
 };
 struct filmain filmdata[N];
-void arxikop(){
-    for ( int i = 0; i <=N; i++)
-    {
-        filmdata[i].code=0;
 
-    }
-    printf("test arxikopoiish");
-    
-}
-void existance(int f_code,int r_value){
-    printf("test existance:%d\n ",filmdata[0].code);
-    for (int i = 0; i <= N; i++)
-    {
-        if(filmdata[i].code==f_code  /*&& r_value==0*/){
-             r_value=1;
-             printf("%d\n", r_value);
-        }
-    }   
-}
-void add(){
-    int r_value=0;
-    printf("test add\n");
-    int f_code;
-    printf("give a code ");
-    scanf("%d",&f_code);
-    existance( f_code,r_value);
-    if(r_value == 0){       
-                printf("give film name ");
-                scanf("%s",filmdata[x].title);
-                printf("give year ");
-                scanf("%d",&filmdata[x].year);
-            }   
-    else if (r_value==1)
-    {
-        printf("this file exist");
-    }
-   main();
-}
-void read(){
-    int s_code; 
-    scanf("%d",&s_code);
-    for(int i=0;i<=N;i++){
-        if (s_code == filmdata[i].code){
-            printf("%s%5d\n",filmdata[i].title,filmdata[i].year);
-            return;
-        }
-        else{
-            if(i==N){
-                printf("film doesn't exist");
-
-            }   }    } 
-}
-void update(){
-    int s_code;
-    ///int n_year= NULL;
-    scanf("%d",&s_code);
-    for(int i=0;i<=N;i++){
-        if (s_code == filmdata[i].code){
-            printf("it's here, put new year");
-           /// scanf("%d", filmdata[i].year);
-        }
-        else{
-            if(i==N){
-                printf("film doesn't exist");
-
-            }
-        }
-}
-}
-void delete(){
-    int s_code;
-    scanf("%d",&s_code);
-     if (s_code == filmdata[x].code){
-        filmdata[x].code=0;
-        filmdata[x].year=0;
-        ///filmdata[x].title[25] = " ";
-        printf("film deleted");
-        }
-    else{
-        if(x==N){
-            printf("film doesn't exist");
-        }
-    }
-} 
-int main(){
+//void arxikop();
+void existance(int f_code,int r_value,int place); 
+void add();
+void read();
+void delete();
+void update();
+int main(void){
     if(metr == 0){
         metr++;
-        arxikop();
-    }
-    
+        //arxikop();
+    }    
     char entolh;
     printf("\ngive order ");
     scanf("%s",&entolh);
@@ -132,4 +52,80 @@ int main(){
         main();
     } 
     return 0;
+}
+/*void arxikop(){
+    for ( int i = 0; i <=N; i++)
+    {
+        filmdata[i].code=0;
+    }
+    printf("test arxikopoiish");   
+}*/
+
+void existance(int f_code,int r_value,int place){
+    printf("test existance:%s\n ",filmdata[0].title);
+    for (int i = 0; i <= N; i++)
+    {
+        if(filmdata[i].code==f_code  && r_value==0){
+             r_value=1;
+             printf("%d\n", r_value);
+             place =i;
+        }
+    }   
+}
+void add(){
+    int place=0;
+    int r_value=0;
+    printf("test add\n");
+    int f_code;
+    printf("give a code ");
+    scanf("%d",&f_code);
+    existance( f_code,r_value,place);
+    if(r_value == 0){      
+                printf("give film name ");
+                scanf("%s",filmdata[x].title);
+                printf("give year ");
+                scanf("%d",&filmdata[x].year);
+            }   
+    else if (r_value==1)
+    {
+        printf("this file exist");
+        place = 0;
+    }
+   main();
+}
+void read(){
+    for(int i=0;i<=N;i++){
+            printf("%s%5d\n",filmdata[i].title,filmdata[i].year);
+        }     
+            }   
+
+void update(){
+    int s_code;
+    scanf("%d",&s_code);
+    for(int i=0;i<=N;i++){
+        if (s_code == filmdata[i].code){
+            printf("it's here, put new year ");
+            scanf("%d", &filmdata[i].year);
+        }
+        else{
+            if(i==N){
+                printf("film doesn't exist");
+            }
+        }
+    }
+}
+void delete(){
+    int s_code;
+    int r_value=0;
+    int place=0;
+    scanf("%d",&s_code);
+    existance(s_code,r_value,place);
+        if(r_value==1){
+        filmdata[place].code=0;
+        filmdata[place].year=0;
+        printf("film deleted");
+        }   
+        else if(r_value ==0){
+            printf("film doesn't exist");
+        }
 }
